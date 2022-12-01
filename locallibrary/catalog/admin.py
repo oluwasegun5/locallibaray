9f25887c897e2dbb58e8f424a8782ce4070461da
oluwasegun5 <adeayosegun5@gmail.com>
2022-12-01 16:20:44 +0100

    User authentication and permissions

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -18,16 +18,17 @@ admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ['book', 'status', 'due_back', 'id']
-    list_filter = ['status', 'due_back']
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
 
     fieldsets = (
-        ('Details', {
+        (None, {
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
+    
 
 admin.site.register(Language)
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Book, BookInstance, Author, Genre, Language
 from django.views import generic
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
 # Create your views here.
 
 def index(request):
@@ -31,7 +32,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_visits': num_visits,
-        
+
     }
 
     return render(request, 'index.html', context=context)
@@ -55,3 +56,16 @@ class AuthorListView(generic.ListView):
 class AuthorDetailView(generic.DetailView):
     """Generic class-based detail view for an author."""
     model = Author
+
+
+    
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
